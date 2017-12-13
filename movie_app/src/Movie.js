e2f9402041a134c5e7d0449d6b7cd242652079a6
Park Hyun Seo <hyeonseo9669@hanmail.net>
2017-12-13 07:04:28 +0000

    Clear 13th Class - Smart vs Dumb

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -3,6 +3,7 @@ import './Movie.css';
 // PropTypes가 Deprecated 되서  prop-types library를 npm으로 설치하여 import
 import PropTypes from 'prop-types';
 
+/*
 class Movie extends Component{
     
     static propTypes = {
@@ -20,7 +21,9 @@ class Movie extends Component{
         );
     }
 }
+*/
 
+/*
 class MoviePoster extends Component{
     
     static propTypes ={
@@ -30,9 +33,38 @@ class MoviePoster extends Component{
     render(){
         return(
 // eslint-disable-next-line
-            <img src={this.props.poster} />
+            <img src={this.props.poster} alt="Movie Poster"/>
         );
     }
+}*/
+
+/*
+    이 컴포넌트는 return을 하기 위해존재 사이클이 존재하지않음
+    한개의 props 만 존재하면 된다. 1개의 props 1개의 html 소스
+*/
+
+function Movie({title, poster}){
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Poster"/>
+        )
+}
+
+Movie.propTypes = {
+    title : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster:PropTypes.string.isRequired
 }
 
 export default Movie;
